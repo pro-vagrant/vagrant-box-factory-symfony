2c994e698777d18326e54362c09c96ef9badfbfb
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-11-13 08:23:44 +0100

    Reformat manifest and update submodules

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,25 +1,25 @@
 stage { 'update':
-  before => Stage['postupdate']
+    before => Stage['postupdate']
 }
 
 stage { 'postupdate':
-  before => Stage['main']
+    before => Stage['main']
 }
 
 class {
-  'ubuntu':   stage => update, action => 'clean';
-  'mysql':    stage => main;
-  'php5':     stage => main;
-  'composer': stage => main;
-  'environment':         stage => main;
-  'php-cs-fixer':        stage => main;
-  'box':                 stage => main;
-  'phpunit':             stage => main;
+    'ubuntu':       stage => update, action => 'clean';
+    'mysql':        stage => main;
+    'php5':         stage => main;
+    'composer':     stage => main;
+    'environment':  stage => main;
+    'php-cs-fixer': stage => main;
+    'box':          stage => main;
+    'phpunit':      stage => main;
 }
 
 # Necessary due to symfony problems with sessions
 # stored inside shared vagrant directory.
 file { '/tmp/sessions':
-  ensure => directory,
-  mode   => 0777,
+    ensure => directory,
+    mode   => 0777,
 }
