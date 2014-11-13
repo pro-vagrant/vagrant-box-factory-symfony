105ce957a5d47181ab63c2b10d9fa249a6a62aa5
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2014-11-13 08:05:36 +0100

    Removed subdirectories: box, phpunit, php-cs-fixer, environment

--- puppet/modules/environment/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class environment {
-
-    exec { 'environment::cd-vagrant':
-        command => "/bin/echo -e \"\\n\\ncd /vagrant \\n\\n\" >> /home/vagrant/.profile",
-        cwd => "/vagrant",
-        path    => '/bin:/usr/bin:/bin:/usr/sbin:/sbin',
-    }
-
-}
