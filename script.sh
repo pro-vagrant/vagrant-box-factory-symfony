5bc08e504e1851bbc84d46b10db1390a2384343f
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-02-05 13:06:21 +0100

    Use gajdaw-symfony puppet module

--- script.sh
+++ script.sh
@@ -18,3 +18,4 @@ sudo puppet module install gajdaw-environment --force
 sudo puppet module install gajdaw-php5 --force
 sudo puppet module install gajdaw-vcsrepository --force
 sudo puppet module install gajdaw-nodejs --force
+sudo puppet module install gajdaw-symfony --force
