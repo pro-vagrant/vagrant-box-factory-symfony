c3ea7509c17a66ab194fbf794e22bdda1316a327
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-02-06 13:53:32 +0100

    Locked versions

--- script.sh
+++ script.sh
@@ -4,18 +4,20 @@ sleep 30
 
 echo "Install Puppet modules..."
 
-sudo puppet module install puppetlabs-stdlib --force
-sudo puppet module install puppetlabs-mysql --force
-sudo puppet module install puppetlabs-apache --force
-sudo puppet module install puppetlabs/concat --force
+sudo puppet module install gajdaw-symfony --force --version 0.2.0
+sudo puppet module install gajdaw-environment --force --version 0.1.2
+sudo puppet module install gajdaw-filefetcher --force --version 0.1.6
+sudo puppet module install gajdaw-nodejs --force --version --version 0.1.2
+sudo puppet module install gajdaw-php5 --force --version --version 0.1.7
+sudo puppet module install gajdaw-php_phars --force --version 0.1.8
+sudo puppet module install gajdaw-ubuntu --force --version 0.1.11
+sudo puppet module install gajdaw-vcsrepository --force --version 0.1.3
 
-sudo puppet module install maestrodev/wget --force
+sudo puppet module install maestrodev/wget --force --version 0.5.7
 
-sudo puppet module install gajdaw-ubuntu --force
-sudo puppet module install gajdaw-php_phars --force
-sudo puppet module install gajdaw-filefetcher --force
-sudo puppet module install gajdaw-environment --force
-sudo puppet module install gajdaw-php5 --force
-sudo puppet module install gajdaw-vcsrepository --force
-sudo puppet module install gajdaw-nodejs --force
-sudo puppet module install gajdaw-symfony --force
+sudo puppet module install puppetlabs/concat --force --version 1.1.2
+sudo puppet module install puppetlabs-mysql --force --version 3.1.0
+sudo puppet module install nanliu/staging --force --version 1.0.3
+sudo puppet module install puppetlabs-stdlib --force --version 4.5.1
+
+
