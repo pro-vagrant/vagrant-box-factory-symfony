efe47ce7d3440b4a4987f154a661e8df217b2b0d
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-06-29 11:16:04 +0200

    Updated modules

--- install-puppet-modules.sh
+++ install-puppet-modules.sh
@@ -10,13 +10,13 @@ echo "Install Puppet modules..."
 #
 
 sudo puppet module install gajdaw-diverse_functions --version 0.1.1 --force
-sudo puppet module install gajdaw-symfony --force --version 0.3.0
-sudo puppet module install gajdaw-environment --force --version 0.1.2
+sudo puppet module install gajdaw-symfony --force --version 0.3.1
+sudo puppet module install gajdaw-environment --force --version 0.1.4
 sudo puppet module install gajdaw-filefetcher --force --version 0.1.6
-sudo puppet module install gajdaw-nodejs --force --version 0.1.2
-sudo puppet module install gajdaw-php5 --force --version 0.1.7
-sudo puppet module install gajdaw-php_phars --force --version 0.1.8
-sudo puppet module install gajdaw-ubuntu --force --version 0.1.11
+sudo puppet module install gajdaw-nodejs --force --version 0.2.2
+sudo puppet module install gajdaw-php5 --force --version 0.3.1
+sudo puppet module install gajdaw-php_phars --force --version 0.2.0
+sudo puppet module install gajdaw-ubuntu --force --version 0.1.13
 sudo puppet module install gajdaw-vcsrepository --force --version 0.1.3
 sudo puppet module install gajdaw-composer_install --force --version 0.3.0
 
