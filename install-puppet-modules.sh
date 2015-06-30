3a789b4964c5b51e799692b016d28daa9bec4169
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-06-30 10:13:43 +0200

    Install phpMyAdmin

--- install-puppet-modules.sh
+++ install-puppet-modules.sh
@@ -10,7 +10,7 @@ echo "Install Puppet modules..."
 #
 
 sudo puppet module install gajdaw-diverse_functions --version 0.1.1 --force
-sudo puppet module install gajdaw-symfony --force --version 2.0.2
+sudo puppet module install gajdaw-symfony --force --version 2.1.0
 sudo puppet module install gajdaw-environment --force --version 0.1.4
 sudo puppet module install gajdaw-filefetcher --force --version 0.1.6
 sudo puppet module install gajdaw-nodejs --force --version 0.2.2
@@ -19,6 +19,7 @@ sudo puppet module install gajdaw-php_phars --force --version 0.2.0
 sudo puppet module install gajdaw-ubuntu --force --version 0.1.13
 sudo puppet module install gajdaw-vcsrepository --force --version 0.1.3
 sudo puppet module install gajdaw-composer_install --force --version 0.3.0
+sudo puppet module install gajdaw-phpmyadmin --force --version 0.3.0
 
 sudo puppet module install maestrodev/wget --force --version 1.5.7
 
@@ -27,5 +28,3 @@ sudo puppet module install puppetlabs/apache --force --version 1.2.0
 sudo puppet module install puppetlabs-mysql --force --version 3.1.0
 sudo puppet module install nanliu/staging --force --version 1.0.3
 sudo puppet module install puppetlabs-stdlib --force --version 4.5.1
-
-
